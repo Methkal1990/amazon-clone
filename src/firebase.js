50dca34fbfee0518cc5d47e9b359b9304c1f8803
Methkal <methkal.khalawi@gmail.com>
2021-01-27 01:38:55 +0100

    starting and styling the app header

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBiHQ6yB-fbqEDPIefP1ea5LR82K2ltWqU",
+  authDomain: "clone-49432.firebaseapp.com",
+  projectId: "clone-49432",
+  storageBucket: "clone-49432.appspot.com",
+  messagingSenderId: "1061041991018",
+  appId: "1:1061041991018:web:149bf8655d70f148718913"
+};
